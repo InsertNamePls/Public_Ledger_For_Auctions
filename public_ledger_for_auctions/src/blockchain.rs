f7e1da9fc0ced8c5164c815595e2217504cd2a72
Jorge Mendes <jorgemendes@Jorges-Air.lan>
2024-03-03 09:45:35 +0000

    add block validation

--- public_ledger_for_auctions/src/blockchain.rs
+++ public_ledger_for_auctions/src/blockchain.rs
@@ -1,8 +1,5 @@
 use std::process::exit;
-
 use sha2::{Digest, Sha256};
-
-use crate::DIFICULTY;
 
 #[derive(Debug)]
 pub struct Block {
@@ -20,28 +17,17 @@ pub struct  Blockchain {
 
 impl Block{
     pub fn new(&self) {}
-    pub fn gen_hash(&self) -> String{
-
-        let blk_data = serde_json::json!({
-            "index": self.index,
-            "prev_hash": self.prev_hash,
-            "hash": self.hash,
-            "nounce": self.nounce,
-            "timestamp": self.timestamp,
-        }
-        );
-        let mut hasher = Sha256::new();
-        hasher.update(blk_data.to_string());
-        let result = hasher.finalize();
-        result.iter().map(|b| format!("{:02x}", b)).collect::<String>()
-    }
 
     pub fn mine_block(&mut self, dificulty: usize){
         let target = "0".repeat(dificulty);
         loop {
             if !self.hash.starts_with(&target) {
                 self.nounce += 1;
-                self.hash = self.gen_hash();
+                self.hash = gen_hash(self.index,
+                                    self.prev_hash.clone(),
+                                    self.nounce,
+                                    self.timestamp,
+                                    self.tx.clone());
             }else {
                 break;
             }
@@ -57,6 +43,20 @@ impl Blockchain {
      }
 }
 
+pub fn gen_hash(index: u32, prev_hash: String, nounce: u64, timestamp: i64, tx: String) -> String{
+    let blk_data = serde_json::json!({
+        "index": index,
+        "prev_hash": prev_hash,
+        "nounce": nounce,
+        "timestamp": timestamp,
+        "tx": tx}
+    );
+    let mut hasher = Sha256::new();
+    hasher.update(blk_data.to_string());
+    let result = hasher.finalize();
+    result.iter().map(|b| format!("{:02x}", b)).collect::<String>()
+}
+
 pub fn validate_block(new_block: &Block, previous_block: &Block, dificulty: usize) {
     if previous_block.hash != new_block.prev_hash {
         println!("previous block hash does not match with new block previous hash.\n expected previous_hash={} got {}",previous_block.hash, new_block.prev_hash);
@@ -64,6 +64,15 @@ pub fn validate_block(new_block: &Block, previous_block: &Block, dificulty: usiz
     }else if !new_block.hash.starts_with(&"0".repeat(dificulty)) {
         println!("hash generated does not contain {} bits of difficulty",dificulty);
         exit(0)
+    }else if   gen_hash(new_block.index,
+                       new_block.prev_hash.clone(),
+                       new_block.nounce,
+                       new_block.timestamp,
+                       new_block.tx.clone()) != new_block.hash {
+
+        println!("hash generated does not match with the one in the block {} ",new_block.hash);
+
+        exit(0);
     }
     else {
         println!("Block {} valid ",new_block.index);
